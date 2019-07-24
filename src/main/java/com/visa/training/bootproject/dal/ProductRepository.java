1e334b8fc2b2152c6c0a2d908a55ac91e04c29de
ravangs <vangalarajiv@gmail.com>
2019-07-24 09:14:00 +0000

    spring boot project and rest

---
+++ src/main/java/com/visa/training/bootproject/dal/ProductRepository.java
@@ -0,0 +1,27 @@
+package com.visa.training.bootproject.dal;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.visa.training.bootproject.domain.Product;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer>{
+	
+	public Product findById(int id);
+	public List<Product> findAll();
+	
+/*
+	public List<Product> findByPrice(float price);
+	public List<Product> findByPriceLessThan(float price);
+	public List<Product> findByName(String name);
+	public List<Product> findByNameLike(String name);
+	
+	@Query("select p frrom Product as p")
+	public List<Product> myComplexQuery();
+
+*/
+}
